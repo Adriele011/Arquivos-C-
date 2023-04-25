4751c27e4d66ffe8d63146efff46794647cd967a
Adriele011 <125559878+Adriele011@users.noreply.github.com>
2023-04-25 11:29:52 -0300

    Add files via upload

---
+++ GerenciaBotoes.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GerenciaBotoes : MonoBehaviour
+{
+    public void Jogar()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Voltar()
+    {
+        SceneManager.LoadScene(0);
+    }
+      public void um()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+       public void voltar()
+    {
+        SceneManager.LoadScene(4);
+    }
+      public void creditos()
+    {
+        SceneManager.LoadScene(3);
+    }
+     
+      public void voltar1()
+    {
+        SceneManager.LoadScene(0);
+    }
+      public void voltar2()
+    {
+        SceneManager.LoadScene(0);
+    }
+      public void xizinho()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void x2()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void dois()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void tres()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void quatro()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void cinco()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void seis()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void sete()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void oito()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void nove()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void dez()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void onze()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void doze()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void treze()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void quartoze()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void quinze()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void voltar3()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void alfabeto1()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void sinais()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    public void numeros()
+    {
+        SceneManager.LoadScene(7);
+    }
+     public void voltar5()
+    {
+        SceneManager.LoadScene(1);
+    }
+     
+     public void voltar6()
+    {
+        SceneManager.LoadScene(1);
+    }
+     public void cores()
+    {
+        SceneManager.LoadScene(8);
+    }
+     public void expressoes()
+    {
+        SceneManager.LoadScene(9);
+    }
+    public void voltar4()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Voltaralfa()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
