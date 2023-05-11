3a716678ed7525f82869892c5efe420c75f697b7
Adriele011 <125559878+Adriele011@users.noreply.github.com>
2023-05-11 07:31:27 -0300

    Atualizado
    Atualizado 11/05

---
+++ Pergunta.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pergunta : MonoBehaviour
+{
+    
+    public string titulo { get; set; }
+     public string pergunta { get; set; }
+     public string resposta { get; set; }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
